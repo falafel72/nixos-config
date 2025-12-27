94e77f53312bbabc1559085f6de7eaeb5f7cc73b
falafel72 <kylehu2000@gmail.com>
2025-12-27 02:37:10 -0800

    update nixos to 25.11

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Flake for OS configuration";
 
   inputs = {
-    # NixOS official package source, using the nixos-25.05 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # NixOS official package source, using the nixos-25.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
